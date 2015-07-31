1dc60b48bf101785df0bfd8b622f9c01eee979de
Bender <plungerman@gmail.com>
2015-07-31 11:43:37 -0500

    updated ORM model to match ron's changes to the database table ccreconjb_rec; updated jbseqno to be primary key so IDs can be generated automatically upon insert/add

--- djczech/reconciliation/data/models.py
+++ djczech/reconciliation/data/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy import Column, BigInteger, Integer, String, DateTime, Float
 
 import datetime
 
@@ -11,17 +11,24 @@ def _get_date():
 class Cheque(Base):
     __tablename__ = 'ccreconjb_rec'
 
-    jbchkno = Column(Integer, primary_key=True)
+    """
+    Integer might not work here since our informix database can
+    have primary key values that are quite large. LongInteger
+    seems to be BigInteger in the SQLAlchemy universe.
+    """
+    jbchkno = Column(BigInteger)
+    jbchknolnk = Column(BigInteger)
     jbimprt_date = Column(DateTime, default=_get_date)
     jbstatus = Column(String)
     jbstatus_date = Column(DateTime)
     jbaction = Column(String)
     jbaccount = Column(String)
     jbamount = Column(Float)
+    jbamountlnk = Column(Float)
     jbissue_date = Column(DateTime)
     jbpostd_dat = Column(String)
     jbpayee = Column(String)
-    jbseqno = Column(Integer)
+    jbseqno = Column(Integer, primary_key=True, autoincrement=True)
 
     def __repr__(self):
         return str(self.jbchkno)
