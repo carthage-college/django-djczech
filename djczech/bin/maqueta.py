89b2387226d498a177cbcc6b885c7a6b0f6a797f
Bender <plungerman@gmail.com>
2015-07-31 11:38:13 -0500

    testing the new python informix library ibm_db_sa; session tests with cheque ORM

--- djczech/bin/maqueta.py
+++ djczech/bin/maqueta.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from djczech.reconciliation.data.models import Cheque
 
 from djzbar.settings import INFORMIX_EARL_TEST as INFORMIX_EARL
@@ -6,6 +8,24 @@ from djzbar.utils.informix import do_sql as do_esql
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy import text
+
+"""
+engine = create_engine(
+    'ibm_db_sa://{}:{}@{}:{}/{}:INFORMIXSERVER=wilson'.format(
+        settings.INFORMIX_USERNAME,
+        settings.INFORMIX_PASSWORD,
+        settings.INFORMIX_HOST,
+        settings.INFORMIX_PORT,
+        settings.INFORMIX_DATABASE
+    )
+)
+
+engine = create_engine('ibm_db_sa://{}:{}@/pyinformix'.format(
+        settings.INFORMIX_USERNAME,
+        settings.INFORMIX_PASSWORD,
+    )
+)
+"""
 
 sql = """
     SELECT
@@ -16,15 +36,19 @@ sql = """
         jbissue_date
 """
 
+#Session = sessionmaker(bind=engine)
+#session = Session()
+# query
+#objs = session.query(Cheque).all()
+
+#objs = engine.execute(sql)
+
 objs = do_esql(sql)
 for o in objs:
-    #print o.jbchkno, o.jbimprt_date
-    print o
+    print o.jbchkno, o.jbimprt_date
+    #print o
 
-"""
-engine = create_engine(INFORMIX_EARL)
-session = sessionmaker(bind=engine, autocommit = True)()
-"""
+#session.close()
 
 #session.query(Cheque).delete()
 #session.commit()
