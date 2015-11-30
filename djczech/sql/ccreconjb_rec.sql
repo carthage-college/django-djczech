bed51bd97eff2884291287514d6c730c07079b64
Bender <plungerman@gmail.com>
2015-11-30 12:17:40 -0600

    replaced wildcard * with each field name

--- djczech/sql/ccreconjb_rec.sql
+++ djczech/sql/ccreconjb_rec.sql
@@ -1,5 +1,21 @@
 SELECT
-    *
+    jbchkno,
+    jbchknolnk,
+    jbimprt_date,
+    jbstatus,
+    jbstatus_date,
+    jbaction,
+    jbaccount,
+    jbamount,
+    jbamountlnk,
+    jbissue_date,
+    jbpostd_dat,
+    jbpayee,
+    jbseqno
 FROM
     train:ccreconjb_rec
-ORDER BY jbseqno DESC LIMIT 10
+WHERE
+    jbaction != ""
+ORDER BY
+    jbimprt_date DESC
+LIMIT 10;
