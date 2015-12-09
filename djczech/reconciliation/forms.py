6b08b21ad9c1e61c728aa5789d3b70a450c41aa1
Bender <plungerman@gmail.com>
2015-12-09 12:53:14 -0600

    new help text; added form field validator for file upload to enforce csv file format

--- djczech/reconciliation/forms.py
+++ djczech/reconciliation/forms.py
@@ -2,15 +2,15 @@
 
 from django import forms
 
+from djtools.fields.validators import MimetypeValidator
+
 class ChequeDataForm(forms.Form):
 
     import_date = forms.DateField()
     bank_data = forms.FileField(
         max_length = "768",
-        help_text = '''
-            <p>Please upload a TSV (Tab Separated file) where the
-            field values are separated by tabs.</p>
-        '''
+        validators=[MimetypeValidator('text/csv')],
+        help_text = '<p>File format: .csv file</p>'
     )
 
     class Meta:
