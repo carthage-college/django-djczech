ecf393731abc1a29cbe1d6134bf73dc2fb5163e2
Bender <plungerman@gmail.com>
2016-03-01 10:53:47 -0600

    added the try/except for better error handling in the apache2 error log

--- djczech/wsgi_default.py
+++ djczech/wsgi_default.py
@@ -1,4 +1,9 @@
-import os, sys
+import os
+import time
+import traceback
+import signal
+import sys
+
 # python
 sys.path.append('/usr/lib/python2.7/')
 sys.path.append('/usr/lib/python2.7/dist-packages/')
@@ -20,5 +25,13 @@ os.environ['LD_LIBRARY_PATH'] = ''
 os.environ['LD_RUN_PATH'] = ''
 # wsgi
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
+    exit(-1)
 
